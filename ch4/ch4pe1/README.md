4d85558439d5a9e9f62ae650415478f80db2616d
JustinMoua <Hmuas11037@gmail.com>
2025-05-21 15:10:38 -0500

    ch4pe1 added.

---
+++ ch4/ch4pe1/README.md
@@ -0,0 +1,6 @@
+Book: C++ Primer Plus
+Author: Stephen Prata
+Chapter: 4
+Problem: 4
+
+![Alt text](.\ch4pe1.png)
---
+++ ch4/ch4pe1/ch4pe1.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+
+char charDowngradeLetterGrade(char charRequestedLetterGrade);
+
+int main(){
+    std::string strFirstName;
+    std::string strLastName;
+    char charGrade;
+    int intAge;
+
+    std::cout << "What is your first name? ";
+    //std::getline(std::cin, strFirstName);
+    std::cin >> strFirstName; 
+    
+    std::cout << "What is your last name? ";
+    std::cin >> strLastName; //Seems to be a problem here. This input seems to have "skipped" (more than likely it is that the newline was still in the buffer and was read. Will have to fix later)
+
+    std::cout << "What letter grade do you deserve? ";
+    std::cin >> charGrade;
+
+    std::cout << "What is your age? ";
+    std::cin >> intAge;
+
+    std::cout << std::endl;
+
+    std::cout << "Name: " << strLastName << ", " << strFirstName << std::endl
+              << "Grade: " << charDowngradeLetterGrade(charGrade) << std::endl
+              << "Age: " << intAge;
+
+    return 0;
+}
+
+char charDowngradeLetterGrade(char charRequestedLetterGrade){
+    
+    char charGivenLetterGrade;
+
+    if ((char)tolower(charRequestedLetterGrade) == 'a'){
+        charGivenLetterGrade = 'B';
+    }else if (((char)tolower(charRequestedLetterGrade) == 'b')){
+        charGivenLetterGrade = 'C';
+    }else if(((char)tolower(charRequestedLetterGrade) == 'c')){
+        charGivenLetterGrade = 'D';
+    }else{
+        charGivenLetterGrade = 'A';
+    }
+    
+    return charGivenLetterGrade;
+}
