1f7d104577ea63767845a98085d44c15221b1bc6
Justin Moua <39628304+JustinMoua@users.noreply.github.com>
2025-05-21 19:01:20 -0500

    ch4pe8 finished. Changes to ch4pe7 to make the example output look nicer.

---
+++ ch4/ch4pe8/ch4pe8.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+struct pizzaPlace{
+    std::string strPizzaCompany;
+    float fltDiameterOfPizza;
+    float fltWeightOfPizza;
+};
+
+int main(){
+
+    pizzaPlace* myOrder = new pizzaPlace{"Dominoes", 12.43, 5.4};
+    std::cout << "Here is a sample pizza order!" << std::endl;
+    std::cout << "Company: " << myOrder->strPizzaCompany << std::endl;
+    std::cout << "Diameter: " << myOrder->fltDiameterOfPizza << std::endl;
+    std::cout << "Weight: " << myOrder->fltWeightOfPizza << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "Now enter your own!" << std::endl;
+    std::cout << "What is the daimeter of the pizza? ";
+    std::cin >> myOrder->fltDiameterOfPizza;
+    std::cout << "What is the pizza company? ";
+    std::getline(std::cin, myOrder->strPizzaCompany);
+    std::cout << "What is the weight of the pizza? ";
+    std::cin >> myOrder->fltWeightOfPizza;
+
+    std::cout << "Company: " << myOrder->strPizzaCompany << std::endl
+              << "Diameter: " << myOrder->fltDiameterOfPizza << std::endl
+              << "Weight: " << myOrder->fltWeightOfPizza << std::endl;
+
+    delete myOrder;
+
+    return 0;
+}
---
+++ ch4/ch4pe8/tempCodeRunnerFile.cpp
@@ -0,0 +1,6 @@
+    pizzaPlace* myOrder = new pizzaPlace{"Dominoes", 12.43, 5.4};
+    std::cout << "Here is a sample pizza order!" << std::endl;
+    std::cout << myOrder->strPizzaCompany << std::endl;
+    std::cout << myOrder->fltDiameterOfPizza << std::endl;
+    std::cout << myOrder->fltWeightOfPizza << std::endl;
+    std::cout << std::endl;
