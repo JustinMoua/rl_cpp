a8b86340cb0ba300e0cf54fcc056385d05fc25b2
Justin Moua <39628304+JustinMoua@users.noreply.github.com>
2025-06-11 08:47:19 -0500

    Began working on ch7pe6

---
+++ ch7/ch7pe6/ch7pe6.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+// void Fill_array(double *begin, double *end);
+// void Show_array(double *begin, double *end);
+// void Reverse_array(double *begin, double *end);
+// void Reverse_array2(double *begin, double *end);
+void Fill_array(double arr[], int arrSize);
+void Show_array(double arr[], int arrSize);
+void Reverse_array(double arr[], int arrSize);
+void Reverse_array2(double arr[], int arrSize);
+
+int main(){
+
+
+    return 0;
+}
+
+void Fill_array(double arr[], int arrSize);
+void Show_array(double arr[], int arrSize);
+void Reverse_array(double arr[], int arrSize);
+void Reverse_array2(double arr[], int arrSize);
